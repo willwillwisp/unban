3fc22101883d26c6f756e7930c385ea41e9b17b6
willwillwisp <willwillwisp@gmail.com>
2023-10-18 19:33:52 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import "dotenv/config";
+import { JWT } from "google-auth-library";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { Telegraf } from "telegraf";
+import sheetsCredentials from "../sheets-credentials.json";
+import { unbanMembersWithExpiredSubscription } from "./unbanMembersWithExpiredSubscription";
+import * as schedule from "node-schedule";
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const GOOGLE_SHEET = process.env.GOOGLE_SHEET;
+
+const CHAT_ID = process.env.CHAT_ID;
+
+const SHEET_NAME = process.env.SHEET_NAME;
+
+if (!BOT_TOKEN || !CHAT_ID || !GOOGLE_SHEET || !SHEET_NAME) {
+  throw new Error(`Please, provide BOT_TOKEN, CHAT_ID, GOOGLE_SHEET, INTERVAL_MINUTES, SHEET_NAME as env vars!`);
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+const serviceAccountAuth = new JWT({
+  email: sheetsCredentials.client_email,
+  key: sheetsCredentials.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const doc = new GoogleSpreadsheet(GOOGLE_SHEET, serviceAccountAuth);
+
+const ruleEvery5Seconds = new schedule.RecurrenceRule();
+const ruleEveryDay = new schedule.RecurrenceRule();
+
+const every5Seconds = new schedule.Range(0, 60, 10);
+ruleEvery5Seconds.second = every5Seconds;
+
+const everyDay = new schedule.Range(0, 6);
+ruleEveryDay.dayOfWeek = everyDay;
+
+void doc.loadInfo().then(() => {
+  void bot.launch();
+
+  schedule.scheduleJob("unban", ruleEvery5Seconds, () => {
+    void unbanMembersWithExpiredSubscription(doc, bot, SHEET_NAME, parseInt(CHAT_ID));
+  });
+
+  console.log("Bot started!");
+  console.log(schedule.scheduledJobs.unban ? "Unban job scheduled!" : "Error while scheduling unban!");
+});
+
+// Enable graceful stop
+process.once("SIGINT", () => {
+  bot.stop("SIGINT");
+  void schedule.gracefulShutdown();
+});
+process.once("SIGTERM", () => {
+  bot.stop("SIGTERM");
+  void schedule.gracefulShutdown();
+});
---
+++ src/unbanMembersWithExpiredSubscription.ts
@@ -0,0 +1,106 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { DateTime } from "luxon";
+import { Telegraf } from "telegraf";
+import { groupBy } from "./utils";
+
+export const unbanMembersWithExpiredSubscription = async (doc: GoogleSpreadsheet, bot: Telegraf, sheetName: string, chatId: number) => {
+  const sheet = doc.sheetsByTitle[sheetName];
+
+  await sheet.loadCells();
+
+  const columnIndex = {
+    id: 0,
+    date: 1,
+  };
+
+  const subscriptionsList = [];
+
+  let rowIndex = 0;
+
+  let row = {
+    id: sheet.getCell(rowIndex, columnIndex.id).value?.toString(),
+    date: sheet.getCell(rowIndex, columnIndex.date).value?.toString(),
+  };
+
+  while (row.id && row.date) {
+    subscriptionsList.push({
+      id: row.id,
+      date: row.date,
+    });
+
+    rowIndex++;
+
+    row = {
+      id: sheet.getCell(rowIndex, columnIndex.id).value?.toString(),
+      date: sheet.getCell(rowIndex, columnIndex.date).value?.toString(),
+    };
+  }
+
+  const groupedSubscriptionList = groupBy(subscriptionsList, (i) => i.id);
+  const subs = Object.values(groupedSubscriptionList);
+
+  for (const sub of subs) {
+    const maxDateSub = sub.reduce((max, game) => (max.date > game.date ? max : game));
+
+    try {
+      const member = await bot.telegram.getChatMember(chatId, parseInt(maxDateSub.id));
+
+      if (member.status !== "kicked" && member.status !== "left") {
+        const subDateJS = new Date(Date.UTC(0, 0, parseFloat(maxDateSub.date) - 1));
+        const subDateLuxon = DateTime.fromJSDate(subDateJS);
+        const now = DateTime.now().minus({ hours: 3 });
+
+        if (subDateLuxon.plus({ month: 1 }) < now) {
+          await bot.telegram.unbanChatMember(chatId, member.user.id);
+          console.log(`Unban ${member.user.id}`);
+
+          await highlightUnbanIdInGoogleSheet(doc, sheetName, member.user.id);
+        }
+      }
+    } catch (err) {
+      // Member not found
+    }
+  }
+};
+
+const highlightUnbanIdInGoogleSheet = async (doc: GoogleSpreadsheet, sheetName: string, id: number) => {
+  const sheet = doc.sheetsByTitle[sheetName];
+
+  await sheet.loadCells();
+
+  const columnIndex = {
+    id: 0,
+    date: 1,
+  };
+
+  let rowIndex = 0;
+
+  let row = {
+    id: sheet.getCell(rowIndex, columnIndex.id),
+    date: sheet.getCell(rowIndex, columnIndex.date),
+  };
+
+  while (typeof row.id.value === "number" || typeof row.id.value === "string") {
+    if (row.id.value.toString() === id.toString()) {
+      const highlightStyle = {
+        rgbColor: {
+          red: 1,
+          green: 0,
+          blue: 0,
+        },
+      };
+
+      row.id.backgroundColorStyle = highlightStyle;
+      row.date.backgroundColorStyle = highlightStyle;
+    }
+
+    rowIndex++;
+
+    row = {
+      id: sheet.getCell(rowIndex, columnIndex.id),
+      date: sheet.getCell(rowIndex, columnIndex.date),
+    };
+  }
+
+  await sheet.saveUpdatedCells(); // save all updates in one call
+};
